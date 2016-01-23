3f7813708d06367367e227822c0bb1f3a53cd2b3
nitikarn <nitikarnboom2030@gmail.com>
2016-01-23 12:06:01 +0700

    portfolio เพิ่มชื่อใต้ภาพ

--- domains/muangthaigood.com/public_html/xn--b3cf9acwn9byep8kya2d/controllers/portfolio.php
+++ domains/muangthaigood.com/public_html/xn--b3cf9acwn9byep8kya2d/controllers/portfolio.php
@@ -1,14 +1,14 @@
 <div id="line"></div>
 <div id="slide">
     <div id="centerslide">
-        <img src="<?=ADDRESS .'img/'. $portfolio_head_image->getDataDescLastID('image', 'id = 1')?>" alt="<?= $portfolio_head_image->getDataDescLastID('alt', 'id = 1')?>" width="812" height="230" />f
+        <img src="<?= ADDRESS . 'img/' . $portfolio_head_image->getDataDescLastID('image', 'id = 1') ?>" alt="<?= $portfolio_head_image->getDataDescLastID('alt', 'id = 1') ?>" width="812" height="230" />f
     </div>
 </div>
 <div id="line1"></div>
 <div id="content">
-  <h1><?= $portfolio_head_txt->getDataDescLastID('title', 'id = 1') ?></h1>
-    <p style="text-align:center;">
-
+    <h1><?= $portfolio_head_txt->getDataDescLastID('title', 'id = 1') ?></h1>
+  
+ <div class="row">
         <?php
         $sql1 = "SELECT * FROM " . $portfolio->getTbl() . " WHERE status = 'ใช้งาน'";
         $query1 = $db->Query($sql1);
@@ -16,19 +16,31 @@
         if ($numRow1 > 0) {
             while ($row1 = $db->FetchArray($query1)) {
                 ?>
-                <a href="<?= ADDRESS ?>img/<?= $row1['image'] ?>" rel="lightbox[example]" title=""><img src="<?= ADDRESS ?>img/<?= $row1['image'] ?>" alt="<?= $row1['alt'] ?>" class="bordered" /></a>
+   
+            <div class="col-md-3">
+                <a href="<?= ADDRESS ?>img/<?= $row1['image'] ?>" rel="lightbox[example]" title="">
+                    <img src="<?= ADDRESS ?>img/<?= $row1['image'] ?>" alt="<?= $row1['alt'] ?>"  class="bordered" style="height: 158px;" />
 
-                <?php
-            }
+                </a>
+                <p class="text-center" style="font-family: Arial;font-size: 12px;">
+                    <?= $row1['name'] ?>
+                </p>
+            </div>
+
+            <?php
         }
-        ?>
-    </p>
+    }
+    ?>
+ </div>
+
+
 
 </div>
 <script>
 
-      initLightbox();
+    initLightbox();
 
-  
+
 
 </script>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
